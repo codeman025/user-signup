65b6e01989a119d26b31a5ea9356311cbb12562c
mk <cody.long.cl21@gmail.com>
2017-07-27 15:19:56 -0700

    updates

--- main.py
+++ main.py
@@ -1,117 +1,83 @@
-from flask import Flask, request, redirect, render_template, session, flash
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, request, redirect, render_template
 
 app = Flask(__name__)
+
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done:beproductive@localhost:8889/get-it-done'
-app.config['SQLALCHEMY_ECHO'] = True
-db = SQLAlchemy(app)
-app.secret_key = 'y337kGcys&zP3B'
+
+    
+def is_name(name):
+    name, username_error = name, ""
+    if name == "":
+        username_error = "No username entered."
+    elif len(name) < 3 or len(name) > 20:
+        username_error = "Username must be between 3 and 20 characters long."
+        name = ""
+    elif " " in name:
+        username_error = "There can be no spaces in the username."
+        name = ""
+    return name, username_error
+
+def is_password(password):
+    password, password_error = password, ""
+    if password == "":
+        password_error = "Enter a password"
+    elif len(password) < 3 or len(password) > 20:
+        password_error = "Password must be between 3 and 20 characters long."
+        password = ""
+    elif " " in password:
+        password_error = "There can be no spaces in the password."
+        password = ""
+    return password, password_error
+
+def is_valid(verify):
+    verify, verify_error = verify, ""
+    password = request.form['password']
+    if password != "":
+        if verify == "" or verify != password:
+            verify_error = "Passwords do not match"
+            verify = ""
+            password = ""
+    return verify, verify_error
+
+def is_email(email):
+    email, email_error = email, ""
+    if email != "":
+        if len(email) < 3 or len(email) > 20:
+            email_error = "Email must be between 3 and 20 characters long."
+            email = ""
+        elif " " in email:
+            email_error = "There can be no spaces in the email address."
+            email = ""
+        elif email.count("@") != 1 or email.count(".") != 1:
+            email_error = "invalid email."
+    return email, email_error
 
 
-class Task(db.Model):
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120))
-    completed = db.Column(db.Boolean)
-    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-    def __init__(self, name, owner):
-        self.name = name
-        self.completed = False
-        self.owner = owner
-
-
-class User(db.Model):
-
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True)
-    password = db.Column(db.String(120))
-    tasks = db.relationship('Task', backref='owner')
-
-    def __init__(self, email, password):
-        self.email = email
-        self.password = password
-
-@app.before_request
-def require_login():
-    allowed_routes = ['login', 'register']
-    if request.endpoint not in allowed_routes and 'email' not in session:
-        return redirect('/login')
-
-
-@app.route('/login', methods=['POST', 'GET'])
+@app.route("/")
 def login():
-    if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
-        user = User.query.filter_by(email=email).first()
-        if user and user.password == password:
-            session['email'] = email
-            flash("Logged in")
-            return redirect('/')
-        else:
-            flash('User password incorrect, or user does not exist', 'error')
-
     return render_template('login.html')
 
 
-@app.route('/register', methods=['POST', 'GET'])
+@app.route("/validate", methods=['POST'])
 def register():
-    if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
-        verify = request.form['verify']
 
-        # TODO - validate user's data
+    name, username_error = is_name(request.form["name"])
+    password, password_error = is_password(request.form["password"])
+    verify, verify_error = is_valid(request.form["verify"])
+    email, email_error = is_email(request.form["email"])
 
-        existing_user = User.query.filter_by(email=email).first()
-        if not existing_user:
-            new_user = User(email, password)
-            db.session.add(new_user)
-            db.session.commit()
-            session['email'] = email
-            return redirect('/')
-        else:
-            # TODO - user better response messaging
-            return "<h1>Duplicate user</h1>"
+    if not username_error and not password_error and not verify_error and not email_error:
+        return render_template('welcome.html', name = name)
+    else:
+        return render_template('login.html', name = name, username_error = username_error, password_error = password_error, 
+        verify_error = verify_error, email = email, email_error = email_error)
+        
 
-    return render_template('register.html')
-
-@app.route('/logout')
-def logout():
-    del session['email']
-    return redirect('/')
+@app.route("/welcome", methods=['POST'])
+def welcome():
+    name = request.form['name']
+    return render_template('welcome.html', title = "welcome", name = name)
 
 
-@app.route('/', methods=['POST', 'GET'])
-def index():
-
-    owner = User.query.filter_by(email=session['email']).first()
-
-    if request.method == 'POST':
-        task_name = request.form['task']
-        new_task = Task(task_name, owner)
-        db.session.add(new_task)
-        db.session.commit()
-
-    tasks = Task.query.filter_by(completed=False,owner=owner).all()
-    completed_tasks = Task.query.filter_by(completed=True,owner=owner).all()
-    return render_template('todos.html',title="Get It Done!", 
-        tasks=tasks, completed_tasks=completed_tasks)
-
-
-@app.route('/delete-task', methods=['POST'])
-def delete_task():
-
-    task_id = int(request.form['task-id'])
-    task = Task.query.get(task_id)
-    task.completed = True
-    db.session.add(task)
-    db.session.commit()
-
-    return redirect('/')
-
-
-if __name__ == '__main__':
-    app.run()
+app.run()
